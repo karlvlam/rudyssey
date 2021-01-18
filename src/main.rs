f4647d09452409bcaeed90e33950b65801c4c18a
Karl Lam <karl.v.lam@gmail.com>
2021-01-18 21:14:34 +0800

    idle connection detection

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ use async_std::io::prelude::*;
 use async_std::io;
 use async_std::net::TcpListener;
 use async_std::net::TcpStream;
-use async_std::sync::{Arc};
-use async_std::sync::channel;
+use async_std::sync::{Arc, Mutex, channel};
 //use async_std::task;
-use async_std::task::spawn;
+use async_std::task::{spawn, sleep};
 use futures::stream::StreamExt;
-//use std::time::Duration;
+use std::time::Duration;
 use std::net::Shutdown;
 use std::string::String;
 use std::collections::HashMap;
@@ -470,6 +469,33 @@ fn kill_connection(stream: TcpStream) {
     }
 }
 
+async fn judge_connection(server_stream: TcpStream, client_stream: TcpStream) {
+
+    let m1 = Arc::new(Mutex::new(Utc::now().timestamp()));
+    let m2 = m1.clone();
+
+    spawn(async move {
+        let mut buffer: [u8; 8] = [0; 8];
+        loop {
+            match client_stream.peek(&mut buffer).await {
+                Ok(byte_count) => {
+                    info!("==== get bytes: {}", byte_count);
+                    *m1.lock().await = Utc::now().timestamp();
+                }
+                Err(e) => {
+                    error!("{:?}", e);
+                }
+            }
+            //sleep(Duration::from_secs(1)).await;
+        }
+    });
+    //info!("{}", t);
+    loop {
+        sleep(Duration::from_secs(1)).await;
+        info!("====== {}", *m2.lock().await);
+    }
+}
+
 async fn handle_connection(client_stream: TcpStream, server_stream: TcpStream, config: &Arc<Config>){
 
     info!("handle_connection!");
@@ -487,16 +513,18 @@ async fn handle_connection(client_stream: TcpStream, server_stream: TcpStream, c
     let client_stream_4 = client_stream.clone();
     let server_stream_4 = server_stream.clone();
 
+    let client_stream_5 = client_stream.clone();
+    let server_stream_5 = server_stream.clone();
     let config = config.clone();
 
 
     spawn(async move {
         info!("[conn] server connected!");
         // !!! very slow, only use for debugging !!!
-        // stream_to_stream(server_stream_2, client_stream_2, "Server").await; 
+        stream_to_stream(server_stream_2, client_stream_2, "Server").await; 
 
         // io::copy -> just forward any data from server to client
-        copy_stream(server_stream_2, client_stream_2, "Server").await;
+        //copy_stream(server_stream_2, client_stream_2, "Server").await;
 
         // close both streams
         //
@@ -516,6 +544,11 @@ async fn handle_connection(client_stream: TcpStream, server_stream: TcpStream, c
         info!("[conn] client disconnected!");
     });
 
+    /*
+    spawn(async move {
+        judge_connection(server_stream_5, client_stream_5).await;
+    });
+    */
 
 }
 
@@ -1326,17 +1359,43 @@ async fn client_to_server(mut client_stream: TcpStream, mut server_stream: TcpSt
     // copy, slow and debug
     async fn stream_to_stream(mut stream_a: TcpStream,mut stream_b: TcpStream, chan:&str) {
         let mut buffer = [0; BUFFER_SIZE];
+
+        let check_time_1 = Arc::new(Mutex::new(Utc::now().timestamp()));
+        let check_time_2 = check_time_1.clone();
+
+        let server_stream = stream_a.clone();
+        let client_stream = stream_b.clone();
+
+        spawn(async move {
+            loop {
+                sleep(Duration::from_secs(5)).await;
+                let current_time = Utc::now().timestamp();
+                let last_time = *check_time_1.lock().await;
+                let diff_time = current_time - last_time;
+                //info!("==== Time diff: {}", diff_time);
+                if (diff_time >= 180) {
+                    kill_connection(server_stream);
+                    kill_connection(client_stream);
+                    info!("==== Time diff > 60, exit!");
+                    break;
+                }
+
+            }
+        });
+
         loop{
             match stream_a.read(&mut buffer).await{
                 Ok(byte_count) => {
+                    *check_time_2.lock().await = Utc::now().timestamp();
                     if byte_count == 0 {
                         // close connection
+                        info!("==== byte count error!");
                         break;
                     }
 
-                    debug!("{} stream=> {}, {:?}", chan, byte_count, buffer);
-                    debug!("==== Reply ====");
-                    debug!("{}", String::from_utf8_lossy(&buffer[0..byte_count]));
+                    //debug!("{} stream=> {}, {:?}", chan, byte_count, buffer);
+                    //debug!("==== Reply ====");
+                    //debug!("{}", String::from_utf8_lossy(&buffer[0..byte_count]));
                     match stream_b.write(&buffer[0..byte_count]).await {
                         _ => {}
                     }
